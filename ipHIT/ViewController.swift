ce59523ba225abe2e9854228657ac84c09329cd0
tatsuaki <tatsuakikuroda@me.com>
2022-02-15 23:30:01 +0900

    Initial Commit

---
+++ ipHIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ipHIT
+//
+//  Created by 黒田建彰 on 2022/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
